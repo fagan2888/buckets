a40d74a90e370adda8467d7e141447cf8044c4bb
ryi <ryi@wayfair.com>
2018-09-17 18:10:48 -0400

    Testing documentation.

--- buckets/bin_data.py
+++ buckets/bin_data.py
@@ -1,24 +1,28 @@
-"""
-bin_data.py 
-
-Different routines for binning data.
-
-scipy.stats.binned_statistic does the same thing as bin_x (but they require edges as input, not centers). Ugh what a waste of time. 
-
-"""
-
 import numpy as np
 
 def standardarray(x):
+    """Make sure an array is a np.array object.
+
+    Parameters
+    ----------
+    x : array-like
+        Array-like object to turn into a numpy array.
+
+    Returns
+    -------
+    x : np.array
+        Converted array.
+
+    """
     if type(x) != np.ndarray:
         x = np.array(x)
     return x
 
 def bin_x(x, y, gx):
-    """ Bins the input data `x` into centers at `gx` and calculates the mean of `y` in those bins.
+    """Bins the input data `x` into centers at `gx` and calculates the mean of `y` in those bins.
 
     Returns
-    =======
+    -------
     bin_means
         Mean of y in each bin.
     bin_std
@@ -43,10 +47,10 @@ def bin_x(x, y, gx):
     gx = np.hstack((gx[0] - binwidth[0]/2., gx[:-1] + binwidth/2., gx[-1] + binwidth[-1]/2))
 
     # NOT DONE: Shift bins so the interval is '( ]' instead of '[ )'.
- 
-    # Determine which bin each y value belongs to. 
+
+    # Determine which bin each y value belongs to.
     binind = np.digitize(x, gx)
-    # Take the sum of each bin and then divide it by the total count in each bin (average). 
+    # Take the sum of each bin and then divide it by the total count in each bin (average).
     bin_means = [y[binind == i].mean() for i in range(1, len(gx))]
     bin_std = [y[binind == i].std() for i in range(1, len(gx))]
     N = [len(y[binind == i]) for i in range(1, len(gx))]
@@ -57,7 +61,7 @@ def bin_n(x, y, n):
     """ Bins the input data `x` into 'n' bins and calculates the mean of `y` in those bins. Each bin contains the same number of elements.
 
     Returns
-    =======
+    -------
     X
         Bin centers.
     Y
