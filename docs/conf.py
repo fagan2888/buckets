873521c1e2d7b5ac2a0431e8706808ec15faf0fa
root <root@DESKTOP-9QP644Q.localdomain>
2018-09-25 09:39:18 -0400

    Attempting to use mock.

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,12 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 
+# Add mock modules.
+import mock
+MOCK_MODULES = ['numpy']
+for mod_name in MOCK_MODULES:
+       sys.modules[mod_name] = mock.Mock()
+
 # -- Project information -----------------------------------------------------
 
 project = 'buckets'
